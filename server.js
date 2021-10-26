2613fcd265a97671b01dc823c7a3c376bb39ca0c
Sriram Nair <shnair1995@gmail.com>
2021-10-26 10:34:03 -0700

    added robar

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 
 const express = require('express')
-
 const path = require('path')
+const rollBar = require('rollbar')
+
+let rollbar = new rollBar({
+    accessToken: 'ab4255df660648819bae1a8947eb2949',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 const app = express()
 
@@ -9,6 +15,7 @@ const app = express()
 console.log(__dirname, path)
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollBar.info('html file served successfully')
 })
 
 const port = process.env.PORT || 4545
